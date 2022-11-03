36da4d8be9ab070e8a34f679c8ebe2e3a6f1ec56
Jalaj <83359199+Jalaj2002@users.noreply.github.com>
2022-11-03 17:46:36 +0530

    Update Draw_Spirogragh.py

--- Draw_Spirogragh.py
+++ Draw_Spirogragh.py
@@ -6,14 +6,14 @@ timmy = Turtle()
 timmy.shape("arrow")   #set turtle pointer
 myScreen = Screen()    
 turtle.colormode(255)   #changing color mode to rgb
-timmy.speed("fastest")
+timmy.speed("fastest")  # setting turtle speed as fastest
 
 
 def random_color():         #function for making a random color by mixing RGB colors
     r = random.randint(0, 255)       # red color
     g = random.randint(0, 255)       # green color
     b = random.randint(0, 255)       # blue color
-    color = (r, g, b)
+    color = (r, g, b)                # Mixing RGB colours as tuple
     return color
 
 
@@ -24,6 +24,6 @@ def draw_spirograph(space_between):           # passing the space between circle
         timmy.setheading(timmy.heading() + space_between)    # rotating angle of pointer
 
 
-draw_spirograph(5)          # function calling with sample input
+draw_spirograph(5)          # Space between circle given as sample input 
 
-myScreen.exitonclick()
+myScreen.exitonclick()     # Screen method to hold the output screen 
